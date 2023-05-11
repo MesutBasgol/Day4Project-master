01594c820b629bde08d0421d03f86eca7c8c2a4e
MesutBasgol <basgol1983@gmail.com>
2023-05-11 09:31:22 +0300

    from the computer at work

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,10 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and .iml file
+        //made initial commit
+        //shared project on github
+        //downloaded this file to other computer
+    }
+}
